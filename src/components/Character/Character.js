023c82d17260808cc7ad0050fae7e1807e7ba4bb
Colin <cburns81592@gmail.com>
2018-12-19 15:09:56 -0500

    some stuff with bootstrap im working on

--- src/components/Character/Character.js
+++ src/components/Character/Character.js
@@ -4,16 +4,32 @@ import Typed from 'react-typed';
 class Character extends Component {
     render() {
         return (
-            <h1>
-                <Typed 
-                    strings={["This is the Character Sheet Page!"]} 
-                    typeSpeed={100}
-                    startDelay={0}
-                    showCursor={true} 
-                />
-            </h1>
+                <div className="row">
+                    <div className="col-6">
+                        <div className="character">
+                            <h1>Character</h1>
+                            <h2>Name: <span>Colin</span></h2>
+                            <h2>Bio: <span>A simple peasant turned hero after saving his village from bandits!</span></h2>    
+                        </div>
+                        
+                    </div>
+                    <div className="col-6">
+                        <h1>Stats</h1>
+                        <h2>Strenght: 1</h2>
+                        <h2>Defense: 1</h2>
+                    </div>
+                </div>
         )
     }
 }
 
 export default Character
+
+            // <h1>
+            //     <Typed 
+            //         strings={["This is the Character Sheet Page!"]} 
+            //         typeSpeed={100}
+            //         startDelay={0}
+            //         showCursor={true} 
+            //     />
+            // </h1>
