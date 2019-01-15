d7b191a989812b77595c81779614b038ffdd76a0
Bill McCutchan <bmccutchanjr@gmail.com>
2019-01-15 14:15:12 -0500

    cleaning up my code

--- src/components/Rankings/Rankings.js
+++ src/components/Rankings/Rankings.js
@@ -1,28 +1,19 @@
 import React, { Component } from 'react';
 import PlayerRanking from './PlayerRanking';
 
-const rankings = require("./ranking.json");
 const axios = require("axios");
 
 class Rankings extends Component
 {   state =
-    {   
-        // standings: rankings
-        standings: []
+    {   standings: []
     }
 
     componentDidMount = () =>
-    // gleep = () =>
-    {
-console.log ("componentDidMount()");
-        // axios.get("https://localhost:8080/api/getRankings")
+    {   // when the component mounts and is React is ready, retrieve ranking data from the server
+
         axios.get("/api/getRankings")
         .then (data =>
         {   
-console.log("axios.get().then()");
-console.log(JSON.stringify(data.data, null, 2));
-// console.log("data[0]: ", data [0]);
-
             this.setState ({ standings: data.data })
         })
         .catch (err =>
@@ -32,17 +23,13 @@ console.log(JSON.stringify(data.data, null, 2));
     
     render()
     {
-        
-console.log("Rankings");
-// this.gleep ();
-        return (
+                return (
             <div className="standing-div">
                 { this.state.standings.map(player =>
                 (   <PlayerRanking
                         name = { player.name }
                         wins = { player.wins }
                         losses = { player.losses }
-                        // percent = { player.wins / (player.wins + player.losses) }
                         percent = { player.pct }
                     />
                 ))}
