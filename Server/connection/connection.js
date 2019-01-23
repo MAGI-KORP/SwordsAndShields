3473aaf1bd409a2888f5908b0257abe56192a249
RichDobrz <rjdobrz@gmail.com>
2019-01-23 01:58:49 -0500

    heroku build

--- Server/connection/connection.js
+++ Server/connection/connection.js
@@ -1,19 +1,19 @@
-const mysql = require("mysql")
+// const mysql = require("mysql")
 
-const connection = mysql.createConnection({
-    host: "localhost",
-    port: 3306,
-    user: "root",
-    password:"root",
-    database: "swords_and_shields_db"
-})
+// const connection = mysql.createConnection({
+//     host: "localhost",
+//     port: 3306,
+//     user: "root",
+//     password:"root",
+//     database: "swords_and_shields_db"
+// })
 
-connection.connect((err) => {
-    if(err) {
-        console.error("error connecting " + err.stack)
-        return
-    }
-    console.log("connected as id + " + connection.threadId)
-})
+// connection.connect((err) => {
+//     if(err) {
+//         console.error("error connecting " + err.stack)
+//         return
+//     }
+//     console.log("connected as id + " + connection.threadId)
+// })
 
-module.exports = connection;
+// module.exports = connection;
--- Server/server.js
+++ Server/server.js
@@ -1,12 +1,14 @@
 const express = require('express')
 const bodyParser = require('body-parser')
+require("dotenv").config()
 const morgan = require('morgan')
 const session = require('express-session')
 const dbConnection = require('./db') 
 const MongoStore = require('connect-mongo')(session)
 const passport = require('./passport');
 const app = express()
-const PORT = 8080
+const PORT = process.env.PORT || 8080
+const socketPORT = process.env.PORT2 || 4001;
 
 // Route requires
 const user = require('./routes/htmlRoutes')
@@ -14,116 +16,116 @@ const api = require("./routes/APIroutes")
 const indexRoutes = require("./routes/index")
 
 //SocketIO
-// const http = require("http");
-// const socketIo = require("socket.io");
-// const axios = require("axios");
-// const server = http.createServer(app);
-// const io = socketIo(server);
+const http = require("http");
+const socketIo = require("socket.io");
+const axios = require("axios");
+const server = http.createServer(app);
+const io = socketIo(server);
 
-//Socket data
-// let log = [];
-// let status = ""
-// let playerCount = 0
-// let playerOne = {};
-// let playerOneMove = ""
-// let playerTwo = {};
-// let playerTwoMove = ""
-// let players = []
-// let sockets = []
-// let activeGame = false
+// Socket data
+let log = [];
+let status = ""
+let playerCount = 0
+let playerOne = {};
+let playerOneMove = ""
+let playerTwo = {};
+let playerTwoMove = ""
+let players = []
+let sockets = []
+let activeGame = false
 
 // Socket Functions
 
-// function sendLog() {
-//   if(log.length > 12){
-//     log = log.slice(-12)
-//   }
-//   io.emit("battleLog", {log: log})  
-// }
+function sendLog() {
+  if(log.length > 12){
+    log = log.slice(-12)
+  }
+  io.emit("battleLog", {log: log})  
+}
 
-// function newGame() {
-//   log = []
-//   if(players.length < 2){
-//     log.push("There is no one in the arena for you to fight, surely someone will come along soon.")
-//     activeGame = false
-//   }
-//   else{
-//     log.push("A new fight will begin as soon as " + players[0] + " and " + players[1] + " choose their moves!")
-//     activeGame = true
-//   }
-//   sendLog()
-//   log = []
-// }
+function newGame() {
+  log = []
+  if(players.length < 2){
+    log.push("There is no one in the arena for you to fight, surely someone will come along soon.")
+    activeGame = false
+  }
+  else{
+    log.push("A new fight will begin as soon as " + players[0] + " and " + players[1] + " choose their moves!")
+    activeGame = true
+  }
+  sendLog()
+  log = []
+}
 
-// function handleChoice(player, move) {
-//   if(player === "Player 1"){
-//     playerOneMove = move
-//   }
-//   if(player === "Player 2"){
-//     playerTwoMove = move
-//   }
-//   if(playerOneMove && playerTwoMove) {
-//     log.push("Player 1 " + " chose to " + playerOneMove + ".")
-//     log.push("Player 2 " + " chose to " + playerTwoMove + ".")
-//     //calculate result
-//     var result = "Something happened!"
-//     log.push(result)
-//     sendLog()
-//     playerOneMove = ""
-//     playerTwoMove = ""
-//   }
-// }
+function handleChoice(player, move) {
+  if(player === "Player 1"){
+    playerOneMove = move
+  }
+  if(player === "Player 2"){
+    playerTwoMove = move
+  }
+  if(playerOneMove && playerTwoMove) {
+    log.push("Player 1 " + " chose to " + playerOneMove + ".")
+    log.push("Player 2 " + " chose to " + playerTwoMove + ".")
+    //calculate result
+    var result = "Something happened!"
+    log.push(result)
+    sendLog()
+    playerOneMove = ""
+    playerTwoMove = ""
+  }
+}
 
-// function joinLobby(socket) {
-//   if(!players.length){
-//     log.push("There is no one in the arena for you to fight, surely someone will come along soon.")
-//   }
-//   sendLog() 
-//   playerCount++
-//   var playerName = "Player " + playerCount
-//   players.push(playerName)
-//   sockets.push(socket)
-//   console.log(players)
-//   io.emit("response", {status: status, players: players, playerName: playerName})
-//   if(sockets.length >= 2 && activeGame === false){
-//     newGame()
-//   }
-// }
+function joinLobby(socket) {
+  if(!players.length){
+    log.push("There is no one in the arena for you to fight, surely someone will come along soon.")
+  }
+  sendLog() 
+  playerCount++
+  var playerName = "Player " + playerCount
+  players.push(playerName)
+  sockets.push(socket)
+  console.log(players)
+  io.emit("response", {status: status, players: players, playerName: playerName})
+  if(sockets.length >= 2 && activeGame === false){
+    newGame()
+  }
+}
 
-// function disconnect(socket) {
-//   var index = sockets.findIndex(function(element){
-//     return element === socket
-//   })
-//   players.splice(index, 1)
-//   sockets.splice(index,1)
-//   if(index = 0 || 1){
-//     newGame()
-//   }
-//   console.log("bye")
-//   io.emit("response", {players: players})
-// }
+function disconnect(socket) {
+  var index = sockets.findIndex(function(element){
+    return element === socket
+  })
+  players.splice(index, 1)
+  sockets.splice(index,1)
+  if(index = 0 || 1){
+    newGame()
+  }
+  console.log("bye")
+  io.emit("response", {players: players})
+}
 
-// io.on("connection", socket => {
-//   var thisSocket = socket
-//   console.log("New client connected")
-//   joinLobby(thisSocket)
+io.on("connection", socket => {
+  var thisSocket = socket
+  console.log("New client connected")
+  joinLobby(thisSocket)
 
-//   socket.on("choice", data => {
-//     if(players.length >= 2){
-//       handleChoice(data.player, data.choice)
-//     }
-//     else{
-//       log.push(data.player + " chose to " + data.choice + (players.length<2 ? " against nobody in particular, there is no longer anyone here...weird.":"."))
-//       sendLog()
-//     }
+  socket.on("choice", data => {
+    if(players.length >= 2){
+      handleChoice(data.player, data.choice)
+    }
+    else{
+      log.push(data.player + " chose to " + data.choice + (players.length<2 ? " against nobody in particular, there is no longer anyone here...weird.":"."))
+      sendLog()
+    }
     
-//   })
+  })
 
-//   socket.on("disconnect", function() {
-//     console.log("Client disconnected")
-//     disconnect(thisSocket)
-//   });
-// });
+  socket.on("disconnect", function() {
+    console.log("Client disconnected")
+    disconnect(thisSocket)
+  });
+});
 
 
 // MIDDLEWARE
@@ -138,7 +140,7 @@ app.use(bodyParser.json())
 // Sessions
 app.use(
 	session({
-		secret: 'fraggle-rock', //pick a random string to make the hash that is generated secure
+		secret: process.env.PW_SECRET || "i@#!}}jidannce}", //pick a random string to make the hash that is generated secure
 		store: new MongoStore({ mongooseConnection: dbConnection }),
 		resave: false, //required
 		saveUninitialized: false //required
@@ -159,3 +161,6 @@ app.use("/", indexRoutes)
 app.listen(PORT, () => {
 	console.log(`App listening on PORT: ${PORT}`)
 })
+
+// SOCKET SERVER
+server.listen(socketPORT, () => console.log(`Listening on port ${socketPORT}`));
