e41ec778bf212795f1f61b16b3a3a6b1b12d5502
RichDobrz <rjdobrz@gmail.com>
2019-01-23 12:59:07 -0500

    enpoint for socketio arena

--- Client/src/components/Arena/Arena.js
+++ Client/src/components/Arena/Arena.js
@@ -9,7 +9,7 @@ class Arena extends Component {
         slot: 0,
         log:[],
         players: [],
-        endpoint: "http://127.0.0.1:4001",
+        endpoint: "https://swords-and-shields.herokuapp.com/",
         socket: false,
         one: {
             name: "Colin the Cat-Lord",
