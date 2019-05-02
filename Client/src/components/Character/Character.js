2ef23e689fcbff35836474fc3e485d75787eba3c
RichDobrz <rjdobrz@gmail.com>
2019-05-02 19:42:16 -0400

    styling on character create screen update.

--- Client/src/components/Character/Character.js
+++ Client/src/components/Character/Character.js
@@ -132,15 +132,14 @@ class Character extends Component {
                                         onChange={this.handleChange} />
                                 </div>
 
-                                <div className="col-12">
+                                <div className="col-12 stat-remain">
                                     <h2>Stat Points Remaining: {this.state.statsRemaining}</h2>
                                 </div>
 
-                                <div className="col-4">
+                                <div className="col-4 mx-auto">
                                     <h2>Strength</h2>
                                     <div className="row">
                                         <div className="form-group col-4 mx-auto">
-                                            <label htmlFor="strength"></label>
                                             <button type="button" className="btn btn-secondary" id="strength" onClick={this.statIncrease}>+</button>
                                             <div>
                                                 {this.state.strength}
@@ -150,11 +149,10 @@ class Character extends Component {
                                     </div>
                                 </div>
 
-                                <div className="col-4">
+                                <div className="col-4 mx-auto">
                                     <h2>Defense</h2>
                                     <div className="row">
                                         <div className="form-group col-4 mx-auto">
-                                        <label htmlFor="defense"></label>
                                         <button type="button" className="btn btn-secondary" id="defense" onClick={this.statIncrease}>+</button>
                                         <div>
                                             {this.state.defense}
@@ -163,11 +161,10 @@ class Character extends Component {
                                     </div>
                                 </div>
                                 </div>
-                                <div className="col-4">
+                                <div className="col-4 mx-auto">
                                     <h2>Evasion</h2>
                                     <div className="row">
                                         <div className="form-group col-4 mx-auto">
-                                            <label htmlFor="evasion"></label>
                                             <button type="button" className="btn btn-secondary" id="evasion" onClick={this.statIncrease}>+</button>
                                             <div>
                                                 {this.state.evasion}
